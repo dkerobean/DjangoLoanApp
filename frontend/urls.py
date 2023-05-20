4191fb296175956b3cf9da5a9f4e9ed8f9672bcb
dickson <dkerobean@gmail.com>
2023-05-20 15:43:58 +0000

    added pages views and urls

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.indexPage, name="index-page"), 
+    path('', views.indexPage, name="index-page"),
+    
+    path('get_loan/', views.getLoan, name="get-loan"),
+    path('loan/', views.loanApplication, name="loan-application"),
+    
+    path('contact/', views.contact, name="contact") 
+    
     
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -5,3 +5,18 @@ from django.shortcuts import render
 def indexPage(request):
     
     return render(request, 'frontend/index.html')
+
+
+def getLoan(request):
+    
+    return render(request, 'frontend/get_loan.html')
+
+
+def loanApplication(request):
+    
+    return render(request, 'frontend/loan_application')
+
+
+def contact(request):
+    
+    return render(request, 'frontend/contact.html')
