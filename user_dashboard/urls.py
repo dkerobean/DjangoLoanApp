f295a10e9038e1959e588471157ad967b94681ca
dickson <dkerobean@gmail.com>
2023-05-22 16:02:44 +0000

    log in functionality done

--- user_dashboard/urls.py
+++ user_dashboard/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     
+    path('home/', views.indexPage, name="user-home"),
+    
     path('login/', views.loginPage, name="user-login"),
-    path('register/', views.registerPage, name="user-register") 
+    path('register/', views.registerPage, name="user-register"), 
+    
+    
 ]
 
--- user_dashboard/views.py
+++ user_dashboard/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
 
 
+def indexPage(request):
+    
+    return render(request, 'user_dashboard/index.html')
+
+ 
 """ AUTH """
+
 def loginPage(request):
+    
+    if request.user.is_authenticated:
+        return redirect('user-home')
+    
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not exists')
+            
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Logged in succesfully')
+            return redirect('user-home')
+        else:
+            messages.error(request, 'Username or Password incorrect')
+            
     
     return render(request, 'user_dashboard/auth/login.html')
 
