7f43bab54c24edabebafaee256c58c47de81885a
dickson <dkerobean@gmail.com>
2023-05-26 14:15:25 +0000

    Contact for frontend done

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Contact(models.Model):
+    
+    name = models.CharField(max_length=150)
+    email = models.EmailField(max_length=150)
+    subject = models.CharField(max_length=150)
+    message = models.TextField(max_length=500)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+    
+    def __str__(self):
+        return self.name
