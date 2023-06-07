e9c7fb58f80bdd1a5b8a586d9364e4aa48b3a912
dickson <dkerobean@gmail.com>
2023-06-07 12:57:25 +0000

    loan application UI and views done

--- admin_dashboard/urls.py
+++ admin_dashboard/urls.py
@@ -4,4 +4,7 @@ from .import views
 
 urlpatterns = [
     path('home/', views.indexPage, name="admin-dashboard"), 
+    
+    path('all_loans/', views.loanApplicants, name="all-loans"),
+    path('loan_application/<str:pk>/', views.viewApplication, name="view-loan"),
 ]
--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from frontend.models import LoanApplication 
 
 def indexPage(request):
     
     return render(request, 'admin_dashboard/index.html')
+
+
+def loanApplicants(request):
+    
+    all_loans = LoanApplication.objects.all()
+    
+    context = {
+        'all_loans':all_loans,
+    }
+    
+    return render(request, 'admin_dashboard/loan_applicants/all_loans.html', context)
+
+
+def viewApplication(request, pk):
+    
+    user_loan = LoanApplication.objects.get(id=pk)
+    
+    context = {
+        'user_loan':user_loan
+    }
+    
+    return render(request, 'admin_dashboard/loan_applicants/view_loan.html', context)
