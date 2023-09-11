8a41bbfdf2cb31805c819d9514c76a4820dfef0e
Dickson <dkerobean@gmail.com>
2023-09-11 13:08:19 +0000

    bug fix in context processor

--- user_dashboard/context_processors.py
+++ user_dashboard/context_processors.py
@@ -2,6 +2,12 @@ from .models import MessageReply
 
 
 def user_replies_context(request):
+
     user = request.user
-    user_replies = MessageReply.objects.filter(support__user=user)
+    if user.is_authenticated:
+        user_replies = MessageReply.objects.filter(support__user=user)
+    else:
+        # handle unauthenticated user
+        user_replies = None
+
     return {'user_replies': user_replies}
